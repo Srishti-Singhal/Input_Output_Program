c7df677e1af53d9a258bc96b6e313e90ebc02e40
Srishti Singhal <99279286+Srishti-Singhal@users.noreply.github.com>
2022-02-17 22:54:26 +0530

    Add files via upload

---
+++ Input_Output.py
@@ -0,0 +1,12 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+name = input("Enter your name please= ")
+Enter your name please= Srishti
+print("Hello! how are you", name)
+Hello! how are you Srishti
+print(name, ",Thank you for using it")
+Srishti ,Thank you for using it
+print("Hello" + name + "!" + "how are you" + "?")
+HelloSrishti!how are you?
+print("Hello" + " my name is " + name)
+Hello my name is Srishti
