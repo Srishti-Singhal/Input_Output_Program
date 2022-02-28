52809d4b7f0644cbc8a10beaeac22bc92bcd0c4b
Srishti Singhal <99279286+Srishti-Singhal@users.noreply.github.com>
2022-02-28 16:49:39 +0530

    Add files via upload

---
+++ slice_notation.py
@@ -0,0 +1,20 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+"happiness"[::-1]
+'ssenippah'
+"happiness"[3:7: ]
+'pine'
+"happiness"[2:7:-1]
+''
+"happiness"[3:7:-1]
+''
+"happiness"[2:7:1]
+'ppine'
+"happiness"[ :-2: ]
+'happine'
+"happiness"[ :-2:-1]
+'s'
+"happiness"[-6:-3:-1]
+''
+"happiness"[-6:-3: ]
+'pin'
